d01c11bc0495f58878361c4f6ceec25d3f6531bd
xorshine <xorshine@icloud.com>
2017-12-06 11:11:22 +0800

    tableview

--- SomoDemo/SomoDemo/TableViewSkeletonController.m
+++ SomoDemo/SomoDemo/TableViewSkeletonController.m
@@ -14,6 +14,8 @@
 
 @property (strong, nonatomic) NSMutableArray<NSNumber *> * dataSource;
 
+@property (assign, nonatomic,getter=isLoaded) BOOL loaded;
+
 @end
 
 @implementation TableViewSkeletonController
@@ -24,13 +26,31 @@
 	self.tableView.rowHeight = 100;
 	[self.tableView registerNib:[UINib nibWithNibName:@"TableViewCell" bundle:nil] forCellReuseIdentifier:@"id"];
 	
-	self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"完成" style:UIBarButtonItemStylePlain target:self action:@selector(load)];
+//	self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"完成" style:UIBarButtonItemStylePlain target:self action:@selector(load)];
 	
 	self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Logo"]];
 	self.navigationItem.titleView.contentMode = UIViewContentModeScaleAspectFit;
+	
+	self.loaded = NO;
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+	[super viewDidAppear:animated];
+	
+	//随便写一个代表网络请求3秒
+	
+	[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+	
+	__weak typeof(self) weakSelf = self;
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3. * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		__strong typeof(weakSelf) strongSelf = weakSelf;
+		[strongSelf load];
+		[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+	});
 }
 
 - (void)load{
+	self.loaded = YES;
 	for (NSInteger i = 0; i < self.dataSource.count; i++) {
 		self.dataSource[i] = @(NO);
 	}
@@ -40,9 +60,13 @@
 
 #pragma mark - Table view data source
 
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-	return self.dataSource.count;
+	if (self.loaded) {
+		return self.dataSource.count;
+	}
+	
+	//随便多少，cell个数能铺满屏幕即可
+	return 10;
 }
  
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
